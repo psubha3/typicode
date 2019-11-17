9141b6aad1b9fdd7228a2abd2969a3ca67464c2c
X-SUBHASHINI PALANISWAMY <SUBHASHINI.PALANISWAMY@calix.com>
2019-11-17 03:53:09 -0800

    reverted html report changes

--- tests/runner.py
+++ tests/runner.py
@@ -1,5 +1,4 @@
 import unittest
-from HtmlTestRunner import HTMLtestRunner
 
 from test_verify_get_users import GetUsersMethods
 from test_verify_post_of_given_user import UserPostMethods
@@ -13,10 +12,5 @@ suite.addTest(loader.loadTestsFromTestCase(UserPostMethods))
 
 
 runner = unittest.TextTestRunner(verbosity=3)
-outfile = file('Report.html', 'w')
-runner = HTMLTestRunner(stream=outfile,
-                            verbosity=2,
-                            title='Typicode api Report',
-                            description='This is test result of typicode api')
 result = runner.run(suite)
 
