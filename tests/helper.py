dca1338a00e980625c30aaeb7de4d18d5a2e6d4d
X-SUBHASHINI PALANISWAMY <SUBHASHINI.PALANISWAMY@calix.com>
2019-11-16 19:51:29 -0800

    Optimized testcase

--- tests/helper.py
+++ tests/helper.py
@@ -5,3 +5,11 @@ def get_response_for_given_path(json_response, path):
     match_response = jsonpath.jsonpath(json_response, path)
     return match_response
 
+
+def check_given_value_present_in_list(listitem, search_string):
+    if listitem.count(search_string):
+        print(search_string + " is present in the list")
+        return True
+    else:
+        print(search_string + " is not present in the list")
+        return False
--- tests/test_verify_get_users.py
+++ tests/test_verify_get_users.py
@@ -2,7 +2,7 @@ import unittest
 import jsonpath
 
 from create_base_session import SessionWithUrlBase
-from helper import get_response_for_given_path
+from helper import get_response_for_given_path, check_given_value_present_in_list
 
 
 class GetUsersMethods(unittest.TestCase):
@@ -14,62 +14,69 @@ class GetUsersMethods(unittest.TestCase):
         """
         Creates the session for https://jsonplaceholder.typicode.com
         Assign a value to username
+
         """
         base_session = SessionWithUrlBase('https://jsonplaceholder.typicode.com')
         cls.session = base_session
         cls.get_users_api = "/users"
         cls.search_username = 'Samantha'
-        cls.username_path = '$..username'
-        cls.userid_path = '$..id'
+        cls.search_username_key = '$..username'
 
-    def test_01_verify_get_user(self):
+    def test_01_verify_if_given_user_is_present(self):
         """
-        1. makes a GET users api call
-        1. Validates if the response code returned is 200
-        2. Validates if the response received is not an empty list.
+        1. Makes a GET users api call
+        2. Validates if the response code returned is 200
+        3. Validates if the response received is not an empty list
+        4. Validates if the username key is present in response
+        5. Check if the given user is present in response
         """
-        print("Verify get users api")
+        print("Verify if given user is present")
         response = self.session.typicode_get_api(self.get_users_api)
+
+        # Validates if response code is 200
         self.assertEqual(response.status_code, 200, "Received status code " + str(response.status_code) +
                          " instead of 200")
-        self.assertNotEqual(len(response.json()), 0, "No users available")
+
+        # Validates if response is not empty
+        self.assertNotEqual(len(response.json()), 0, "User response is empty")
+
+        # Validates if the username key is present in response
+        username_response = get_response_for_given_path(response.json(), self.search_username_key)
+        self.assertNotEquals(username_response, False, "Searched string is not present")
+
+        # Validates if the given username is present in response
+        user_present = check_given_value_present_in_list(username_response, self.search_username)
+        self.assertTrue(user_present, "Username " + self.search_username + "is not present in the response")
 
     def test_02_get_api_with_user_username(self):
         """
         1. Calls get api with username query
         2. Validates if response code returned is 200
         3. Validates if response received is not an empty list
-        4. Validates if the path is present in the given response
-        5. Validates if the path value is same as our search value
+        4. Validates if the username key is present in response
+        5. Check if the given user is present in response
         """
-        print("Verify get users api with username")
+        print("Verify get users api with username query")
         response = self.session.typicode_get_api_with_query(self.get_users_api, 'username=' + self.search_username)
+        # Validates if response code is 200
         self.assertEqual(response.status_code, 200, "Received status code " + str(response.status_code) +
                          " instead of 200")
-        self.assertNotEqual(len(response.json()), 0, "No response received")
-        username_response = get_response_for_given_path(response.json(), self.username_path)
-        self.assertNotEquals(username_response, False, "Searched string is not present")
-        self.assertItemsEqual(username_response, [self.search_username],
-                              "Username " + self.search_username + "is not present in the response")
 
-    def test_03_retrieve_and_store_userid_of_specific_user(self):
-        print("Store the user id received from the get user api response")
-        response = self.session.typicode_get_api_with_query(self.get_users_api, 'username=' + self.search_username)
-        self.assertEqual(response.status_code, 200, "Received status code " + str(response.status_code) +
-                         " instead of 200")
-        self.assertNotEqual(len(response.json()), 0, "No response received")
-        username_response = get_response_for_given_path(response.json(), self.username_path)
+        # Validates if response is not empty
+        self.assertNotEqual(len(response.json()), 0, "User response is empty")
+
+        # Validates if the username key is present in response
+        username_response = get_response_for_given_path(response.json(), self.search_username_key)
         self.assertNotEquals(username_response, False, "Searched string is not present")
-        self.assertItemsEqual(username_response, [self.search_username],
-                              "Username " + self.search_username + "is not present in the response")
-        userid_response = get_response_for_given_path(response.json(), self.userid_path)
-        self.assertNotEquals(userid_response, False, "Searched string is not present")
+
+        # Validates if the given username is present in response
+        user_present = check_given_value_present_in_list(username_response, self.search_username)
+        self.assertTrue(user_present, "Username " + self.search_username + "is not present in the response")
 
     @classmethod
     def tearDown(cls):
         cls.session.close_typicode_session()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
 
